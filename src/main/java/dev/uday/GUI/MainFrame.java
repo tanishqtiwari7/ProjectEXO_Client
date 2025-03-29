f53d0d18d386deb26a894e6e49643c2fe8edad78
UdayKhare09 <udaykhare77@gmail.com>
2025-03-29 19:42:50 +0530

    Set application icon in main frame

--- src/main/java/dev/uday/GUI/MainFrame.java
+++ src/main/java/dev/uday/GUI/MainFrame.java
@@ -3,6 +3,8 @@ package dev.uday.GUI;
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
 
 public class MainFrame {
     public static JFrame mainFrame;
@@ -15,6 +17,10 @@ public class MainFrame {
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setResizable(false);
 
+        ImageIcon icon = new ImageIcon(Objects.requireNonNull(MainFrame.class.getClassLoader().getResource("icons/logo.png")));
+        // Scale the image to appropriate icon size (64x64 is common for window icons)
+        Image scaledImage = icon.getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH);
+        mainFrame.setIconImage(scaledImage);
 
         LoginPanel.setLoginPanel();
         mainFrame.add(LoginPanel.loginPanel);
